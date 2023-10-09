2a4819fa2061d252d42038823c4d66079f975e22
Alrrep <juan.daniel.rangel.avila@gmail.com>
2023-10-09 12:57:49 -0600

    maximum actualizado

--- maximum.py
+++ maximum.py
@@ -2,11 +2,12 @@
 #maximum algorithm
 #by: juan.daniel.rangel.avila@gmail.com
 #LICENSE GNU/GPL
+import sys
 data = [1.0, 3.14, 6.2, 8.1, 5.3]
 
-maximum -999999.99
-for x in maximum:
-if x>maximum
-    maximum=x
-    #print(x)
+maximum = -sys.float_info.max
+for x in data:
+	if x>maximum:
+		maximum=x
+    		#print(x)
 print(maximum)
