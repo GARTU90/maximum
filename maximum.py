034b36c12693ff1b8254982af2c784b00d50b160
Alrrep <juan.daniel.rangel.avila@gmail.com>
2023-10-09 12:40:48 -0600

    maximum

---
+++ maximum.py
@@ -0,0 +1,12 @@
+#i/usr/bin/env python3
+#maximum algorithm
+#by: juan.daniel.rangel.avila@gmail.com
+#LICENSE GNU/GPL
+data = [1.0, 3.14, 6.2, 8.1, 5.3]
+
+maximum -999999.99
+for x in maximum:
+if x>maximum
+    maximum=x
+    #print(x)
+print(maximum)
