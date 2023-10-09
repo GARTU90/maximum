297401cffc243237bb06943967992a674fcae6a2
Alrrep <juan.daniel.rangel.avila@gmail.com>
2023-10-09 12:45:19 -0600

    minimum

--- minimum.py
+++ minimum.py
@@ -6,7 +6,7 @@ data = [1.0, 3.14, 6.2, 8.1, 5.3]
 
 maximum = 999999.99
 for x in maximum:
-if x<maximum
-    maximum=x
-    #print(x)
+    if x<maximum:
+        maximum=x
+        #print(x)
 print(maximum)
